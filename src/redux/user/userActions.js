7266668162565b7e81273e8ab514d5da3a7eedbf
Saheel Bharti <saheel.bharti@sofocle.com>
2020-04-17 15:20:29 +0530

    first commit

---
+++ src/redux/user/userActions.js
@@ -0,0 +1,37 @@
+import { FETCH_USER_REQUEST, FETCH_USER_SUCCESS, FETCH_USER_FAILURE } from './userTypes'
+import axios from 'axios'
+export const fetchUserRequest = () => {
+    return {
+        type: FETCH_USER_REQUEST
+    }
+}
+
+export const fetchUserSuccess = users => {
+    return {
+        type: FETCH_USER_SUCCESS,
+        payload: users
+    }
+}
+
+export const fetchUserFailure = error => {
+    return {
+        type: FETCH_USER_FAILURE,
+        payload: error
+    }
+}
+
+export const fetchUsers = () => {
+    return function(dispatch) {
+        dispatch(fetchUserRequest());
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(response => {
+            // response.data is the array of users
+            const users = response.data//.map(user => JSON.stringify(user));
+            dispatch(fetchUserSuccess(users));
+        })
+        .catch(error => {
+            // error.message is the error description
+            dispatch(fetchUserFailure(error.message));
+        })
+    }
+}
